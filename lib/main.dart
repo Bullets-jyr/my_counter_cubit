a6278ddffba503db5fd4414d4a98923cee24427e
Bullets-jyr <jyr@bullets.co.kr>
2024-01-02 11:02:45 +0900

    6. Injecting Cubits/Blocs into widget tree and Accessing Cubits/Blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_counter_cubit/cubits/counter/counter_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Counter Cubit',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'My Counter Cubit',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
@@ -28,7 +33,7 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Text(
-          '0',
+          '${BlocProvider.of<CounterCubit>(context).state.counter}',
           style: TextStyle(fontSize: 52.0),
         ),
       ),
@@ -36,13 +41,17 @@ class MyHomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              BlocProvider.of<CounterCubit>(context).increment();
+            },
             child: Icon(Icons.add),
             heroTag: 'increment',
           ),
           SizedBox(width: 10.0),
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              BlocProvider.of<CounterCubit>(context).decrement();
+            },
             child: Icon(Icons.remove),
             heroTag: 'decrement',
           ),
